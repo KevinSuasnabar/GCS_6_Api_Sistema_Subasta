66e44168bf3ad3254ac3dcb2778dcbba16a700cc
Sulca97 <raul_97_02@hotmail.com>
2020-10-27 17:41:36 -0500

    Test12

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -57,9 +57,9 @@ const uploadPhoto = async(req = request, res = response) => {
                 await User.findByIdAndUpdate(id, { img: result.url })
             });
 
-            // if (fs.existsSync(pathName)) {
-            //     fs.unlinkSync(pathName)
-            // }
+            if (fs.existsSync(pathName)) {
+                fs.unlinkSync(pathName)
+            }
 
             return res.status(200).json({
                 path: pathName,

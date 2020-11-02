88e85a5df45ff8cc2c480994d926f5529fb10b33
Sulca97 <raul_97_02@hotmail.com>
2020-11-01 19:05:03 -0500

    Registro de productos y listado de productos por usuario listo

--- src/routes/upload.route.js
+++ src/routes/upload.route.js
@@ -3,7 +3,7 @@
 */
 
 const { Router } = require('express');
-const { uploadPhoto } = require('../controllers/upload.controller');
+const { uploadPhoto, uploadPhotoProduct } = require('../controllers/upload.controller');
 const { validationJWT } = require('../middlewares/validation-jwt.middleware');
 const fileUpload = require('express-fileupload');
 const router = Router();
@@ -11,5 +11,6 @@ const router = Router();
 router.use(fileUpload());
 
 router.post('/user/:id', [validationJWT], uploadPhoto);
+router.post('/product', [validationJWT], uploadPhotoProduct);
 
 module.exports = router;

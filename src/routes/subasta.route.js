735d78477f7635b1f7a89d46dc5baf2298b41a65
Ramiro <echoram123@gmail.com>
2021-01-12 19:31:44 -0500

    cambios menores

--- src/routes/subasta.route.js
+++ src/routes/subasta.route.js
@@ -3,7 +3,9 @@
 */
 
 const { Router } = require('express');
-const { subastaPorCategoria, createSubasta, getSubastas, getSubastaById, getSubastasByIdComprador, calificarSubasta, getHistorialPujasBySubasta, pujarSubasta, getSubastasByParticipacion, finalizarSubasta } = require('../controllers/subasta.controller');
+const { subastaPorCategoria, createSubasta, getSubastas, getSubastaById, getSubastasByIdComprador, calificarSubasta, getHistorialPujasBySubasta,
+     pujarSubasta, getSubastasByParticipacion, finalizarSubasta,
+      filtrarSubastasByProductoAndVendedor, getPujasBySubasta } = require('../controllers/subasta.controller');
 const { validationJWT } = require('../middlewares/validation-jwt.middleware');
 const router = Router();
 
@@ -15,7 +17,9 @@ router.get('/participaciones', [validationJWT], getSubastasByParticipacion);
 router.put('/calificar', [validationJWT], calificarSubasta);
 router.post('/pujar/:id', [validationJWT], pujarSubasta);
 router.post('/:idProducto/vendedor/:idVendedor', [validationJWT], createSubasta);
-router.put('/finalizar/:idSubasta', finalizarSubasta);
-router.get('/category/:categoryName', subastaPorCategoria)
+router.put('/finalizar/:idSubasta', [validationJWT], finalizarSubasta);
+router.get('/category/:categoryName', [validationJWT], subastaPorCategoria)
+router.get('/filtrarSubastasByProductoAndVendedor/:filter', [validationJWT], filtrarSubastasByProductoAndVendedor)
+router.get('/getPujasBySubasta/:idSubasta', [validationJWT], getPujasBySubasta);
 
 module.exports = router;
